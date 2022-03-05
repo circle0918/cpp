f26c96ad6353c3e87f4f0224f08a83ad1e755d63
circle0918 <evelyne.yuanyuan@gmail.com>
2022-03-05 21:16:06 +0100

    tod

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -13,29 +13,60 @@ class Convert
 {
 public:
     Convert(char c);
+    Convert(float f);
     void print();
+    void setImpossible(bool im);
 private:
     char _c;
     int _i;
     float _f;
     double _d;
-    bool flag_char_impossible;
-    bool flag_char_non_displayable;
-    bool flag_int_impossible;
+    bool _isImpossible;
 };
 
-Convert::Convert(char c) : _c(c)
+Convert::Convert(char c) : _c(c), _isImpossible(false)
 {
     _i = static_cast<int>(c);
     _f = static_cast<float>(c);
     _d = static_cast<double>(c);
 }
 
+Convert::Convert(float f) : _f(f), _isImpossible(false)
+{
+    _c = static_cast<char>(f);
+    _i = static_cast<int>(f);
+    _d = static_cast<double>(f);
+}
+
+static bool isZero(const std::string& s)
+{
+    for(size_t i = 0; i < s.length(); i++)
+        if (!(s[i] == '0' || s[i] == '.'))
+            return false;
+    return true;
+}
+
+static bool isImpossible(const std::string& s)
+{
+    return (s == "inf" || s == "+inf" || s == "-inf"
+            || s == "nan" || s == "+nan" || s == "-nan");
+}
+
 void
 Convert::print()
 {
-    std::cout << "char : '" << _c << "'" << std::endl;
-    std::cout << "int : " << _i << std::endl;
+    if (_isImpossible)
+        std::cout << "char : impossible" << std::endl;
+    else if (_c < 32 || _c > 126)
+        std::cout << "char : non displayable" << std::endl;
+    else
+        std::cout << "char : '" << _c << "'" << std::endl;
+    
+    if (_isImpossible)
+        std::cout << "int : impossible" << std::endl;
+    else
+        std::cout << "int : " << _i << std::endl;
+
     if (_f - (int)_f != (float)0)
         std::cout << "float : " << _f << "f" << std::endl;
     else
@@ -46,32 +77,36 @@ Convert::print()
         std::cout << "double : " << _d << ".0" << std::endl;
 }
 
+void
+Convert::setImpossible(bool im)
+{
+    _isImpossible = im;
+}
+
 void detectType(const std::string& str)
 {
-    std::cout << "bol: " << str << " length : " << str.length() << std::endl;
-    
     if (str.length() == 3 && str[0] == '\'' && str[2] == '\'') {
         char param = str[1];
         Convert c_char = Convert(param);
         c_char.print();
         return;
-    //}
-    // if (str.back() == 'f') {
-    //     double d = strtod(str.substr(0, str.size()-1).c_str(), NULL);
-    //     if (d != 0.0)
-    //     {
-    //         float param = (float)d;
-    //         Convert c_float = Convert(param);
-    //         c_float.print();
-    //         return;
-    //     }
-    // }
-
-    // if (str.find('.') != std::string::npos)
-
-    } else {
-        std::cout << "wrong type" << std::endl;
     }
+    if (str[str.length()-1] == 'f') {
+        const std::string ss = str.substr(0, str.size()-1);
+        double d = strtod(ss.c_str(), NULL);
+        if ((d == 0.0 && isZero(ss))
+            || d != 0.0)
+        {
+            float param = (float)d;
+            Convert c_float = Convert(param);
+            c_float.setImpossible(isImpossible(ss));
+            c_float.print();
+            return;
+        }
+    }
+    if (str.find('.') != std::string::npos)
+
+    std::cout << "wrong type" << std::endl;
 }
 
 int main(int ac, char **av)
